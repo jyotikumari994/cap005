593cc9f633c637d8749b324393f25b42dad50fa6
jyotikumari994 <jyotikumaripatwa94@gmail.com>
2023-09-10 13:38:10 +0530

    added prime.js

---
+++ prime.js
@@ -0,0 +1,17 @@
+let number =5;
+let fact=0;
+for(let i=1;i<=number;i++)
+{
+ if(number%i==0){
+    fact++;
+ }
+ }
+ if(fact==2)
+ {
+
+ 
+console.log("Prime")
+}
+else{
+console.log("Not Prime")
+}
